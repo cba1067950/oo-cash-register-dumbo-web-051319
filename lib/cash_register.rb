73f5bd2b1d211d36a9ef2e122aed63677a2e35e0
Joe Jamsky <cba1067950@yahoo.com>
2019-05-15 19:14:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,7 @@ class CashRegister
     @quantity = 0
   end
   
-  def add_item(title, price, quantity = 0)
+  def add_item(title, price, quantity = 1)
     @total += price
     @quantity = quantity
   end 
