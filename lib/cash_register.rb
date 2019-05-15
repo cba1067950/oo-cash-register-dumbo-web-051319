1c8533c96de4b1efa31be375ab04fd6ded6ffe3c
Joe Jamsky <cba1067950@yahoo.com>
2019-05-15 19:31:37 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,7 +15,7 @@ class CashRegister
   end 
   
   def apply_discount()
-    @total = @total/@discount - @total
+    @total =- @total/(@discount*0.01)
     
     if @discount == 0 
       return "Error message"
