519a3f37ae2ae1aa7c8e5c59936dd4c17589f281
Joe Jamsky <cba1067950@yahoo.com>
2019-05-15 19:18:10 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   
   def add_item(title, price, quantity = 1)
     @total += price * quantity
-    @list << "#{title}: $#{price}   count#{quantity}"
+    for 
   end 
   
   def apply_discount()
