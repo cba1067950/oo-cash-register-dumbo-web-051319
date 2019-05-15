43fe2278e89b541e6c407fe75b99c8144b1f3cbd
Joe Jamsky <cba1067950@yahoo.com>
2019-05-15 18:56:15 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,12 @@
+require 'pry'
+
 class CashRegister
   #attr_accessor :total :discount
   
   def initialize(discount = 20) 
     @total = 0
     @discount = discount
+    binding.pry 
   end
   
   
