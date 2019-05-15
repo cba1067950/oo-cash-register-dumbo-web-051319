79e3e15ac6417d91b74ec74104674fa2450afdee
Joe Jamsky <cba1067950@yahoo.com>
2019-05-15 19:23:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,6 +11,7 @@ class CashRegister
   
   def add_item(title, price, quantity = 1)
     @total += price * quantity
+    binding.pry 
     self.list.fill(tile, list.size, quantity)
     binding.pry
   end 
