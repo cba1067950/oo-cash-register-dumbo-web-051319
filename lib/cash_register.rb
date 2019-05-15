ce7b479fe2461171cdcc914018636495be2762c8
Joe Jamsky <cba1067950@yahoo.com>
2019-05-15 19:33:43 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,12 +15,12 @@ class CashRegister
   end 
   
   def apply_discount()
-    @total =- @total/(@discount*0.01)
+    @total -= @total/(@discount*0.01)
     
-    if @discount == 0 
-      return "Error message"
-    end 
-    return "Success"
+    if @discount > 0  
+      return "After the discount, the total comes to $#{total.to_i}."
+    else 
+      return "There is no disount to apply"
   end 
   
   def items
