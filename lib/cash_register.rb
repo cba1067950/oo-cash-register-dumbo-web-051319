a2ee6139c71dab5dcb9715e8671bb835aec120e6
Joe Jamsky <cba1067950@yahoo.com>
2019-05-15 19:35:37 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,7 +20,7 @@ class CashRegister
     if @discount > 0  
       return "After the discount, the total comes to $#{@total.to_i}."
     else 
-      return "There is no disount to apply"
+      return "There is no discount to apply"
     end 
   end 
   
